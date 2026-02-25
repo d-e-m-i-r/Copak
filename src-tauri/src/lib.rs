465c06441cb16c69c398cf4969f620c2acdc4d0e
4mir7z <166256122+4mir7z@users.noreply.github.com>
2026-02-25 19:52:22 +0330

    Implement Background Clipboard Listener  (#6)
    * chore: hide main window on startup
    
    * chore: add tauri-plugin-clipboard-manager to tauri.config.json
    
    * feat: implement background clipboard listener in lib.rs
    
    * fix: avoid repeated errors for none-text content
    
    * refactor: optimize clipboard comparison using hashing
    
    * chore: extract duplicated part  into a separate helper function
    
    ---------
    
    Co-authored-by: “AmirJZ” <“amir.jzdh@gmail.com”>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,10 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::thread;
+use std::time::Duration;
+use tauri::Manager; // for method app.handle()
+use tauri_plugin_clipboard_manager::ClipboardExt; // for method  handle.clipboard().read_text()
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -20,7 +26,60 @@ pub fn run() {
         .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            monitor_clipboard(app_handle);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn monitor_clipboard(app: tauri::AppHandle) {
+    thread::spawn(move || {
+        // use hash to avoid String Cloning
+        let mut last_hash: u64 = 0;
+        if let Ok(initial_clipboard_content) = app.clipboard().read_text() {
+            if !initial_clipboard_content.is_empty() {
+                last_hash = get_clipboard_content_hash(&initial_clipboard_content);
+            }
+        }
+        // continuously monitor clipboard content changes
+        loop {
+            match app.clipboard().read_text() {
+                Ok(current_clipboard_content) => {
+                    let mut current_hash: u64 = 0;
+                    if !current_clipboard_content.is_empty() {
+                        current_hash = get_clipboard_content_hash(&current_clipboard_content);
+                    }
+                    if current_clipboard_content.len() > 0 {
+                        if current_hash != last_hash {
+                            println!("Clipboard content changed: {}", current_clipboard_content);
+                            last_hash = current_hash;
+                        };
+                    } else {
+                        // If the clipboard is empty, we can consider it as a change
+                        if last_hash != 0 {
+                            println!("Clipboard content cleared");
+                            last_hash = 0; // Reset hash for empty content
+                        }
+                    }
+                } //debugging purpose
+                Err(e) => {
+                    if last_hash != 0 {
+                        eprintln!("Failed to read clipboard content: {}", e);
+                        last_hash = 0; // Reset hash on error to detect future changes
+                    }
+                }
+            }
+            thread::sleep(Duration::from_secs(1)); // Check every second
+        }
+    });
+}
+
+fn get_clipboard_content_hash(clipboard_content: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    clipboard_content.hash(&mut hasher);
+    hasher.finish()
 }
